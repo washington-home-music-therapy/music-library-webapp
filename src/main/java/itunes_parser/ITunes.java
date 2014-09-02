217ec13c8d2536facdb366a1faec7a0bf323e013
karl-alex-pauls <karl.alex.pauls@gmail.com>
2014-09-02 09:54:17 -0700

    iterated on csv output

--- src/main/java/itunes_parser/ITunes.java
+++ src/main/java/itunes_parser/ITunes.java
@@ -7,23 +7,68 @@ import itunes_parser.itunes.MusicTrack;
 import itunes_parser.itunes.MusicTrackReference;
 import itunes_parser.plist.PLEventReader;
 import itunes_parser.plist.PLUtil;
+import lombok.Data;
 import org.apache.commons.beanutils.BeanUtils;
 
 import javax.xml.stream.XMLStreamException;
 import java.io.*;
 import java.lang.reflect.InvocationTargetException;
+import java.text.MessageFormat;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
 /**
  * ITunes reader
  */
 public final class ITunes {
+    private static final boolean DEBUG = false;
     private static final SimpleDateFormat DATE = new SimpleDateFormat("YYYYMMdd");
 
-    private final ImmutableList<String> display;
-    private final BeanPropertyComparator sort;
+//    private final ImmutableList<String> display;
+//    private final BeanPropertyComparator sort;
 
+    public static Map<Genre,Config> configuration(Multimap<Genre, Genre> groups) {
+        Map<Genre,Config> configs = new HashMap<>();
+        configs.put(Genre.NIL,new Config());
+
+        Config c = new Config();
+        c.display.add("genre");
+        c.sort = new BeanPropertyComparator(ImmutableList.of("artist","name"));
+
+        for(Genre grouping : groups.keySet()) {
+            configs.put(grouping, c);
+        }
+
+        c = new Config();
+        c.display = ImmutableList.of("composer", "name");
+        c.sort = new BeanPropertyComparator(ImmutableList.of("composer", "name"));
+        configs.put(new Genre("Classical", "Classical"), c);
+
+//        c = new Config();
+//        c.display = ImmutableList.of("composer", "name");
+//        configs.put(new Genre("Orchestra", "Popular"), c);
+//        configs.put(new Genre("Patriotic", "Popular"), c);
+
+        c = new Config();
+        c.sort = new BeanPropertyComparator(ImmutableList.of("album", "name"));
+        configs.put(new Genre("Soundtrack", "Popular"), c);
+
+        Genre spiritualGroup = new Genre("","Spiritual");
+        c = new Config();
+        c.display = ImmutableList.of("name");
+        c.sort = new BeanPropertyComparator(ImmutableList.of("genre","name"));
+        for(Genre g : groups.get(spiritualGroup)) {
+            configs.put(g, c);
+        }
+        c = new Config();
+        c.display = ImmutableList.of("genre","name");
+        c.sort = new BeanPropertyComparator(ImmutableList.of("genre","name"));
+        configs.put(spiritualGroup, c);
+
+        return configs;
+    }
 
     public static void main(String[] argv) throws Throwable {
         File errorF = new File(ITunes.class.getName() + "_errors_" + DATE.format(new Date()) + ".txt");
@@ -36,28 +81,73 @@ public final class ITunes {
             if(file.exists() && file.canRead()) break;
         }
         try {
-            ITunes itReader = new ITunes("*genre", "year", "*grouping", "name", "artist", "album", "*trackNumber");
+            ITunes itReader = new ITunes();
             MusicLibrary library = itReader.read(file);
-            ImmutableListMultimap<String,MusicTrack> genres = Multimaps.index(
-                    library.getTracks().values(), track -> track.getGenre());
+            final ImmutableListMultimap<Genre,MusicTrack> genres = Multimaps.index(
+                    library.getTracks().values(), it -> new Genre(it.getGenre().trim(), it.getGrouping().trim()));
+            final ImmutableListMultimap<Genre,MusicTrack> groups = Multimaps.index(
+                    library.getTracks().values(), it->new Genre("",it.getGrouping().trim()));
+            final ImmutableListMultimap<Genre,Genre> groupsToGenres = Multimaps.index(
+                    genres.keySet(), it->new Genre("",it.getGrouping().trim()));
+
+            Map<Genre,Config> configuration = configuration(groupsToGenres);
 
             File completeFile = getNumberedFile("complete-library-" + DATE.format(new Date()), ".csv");
+            File indexFile = getChildFile(completeFile, "index.html");
+            PrintWriter index = new PrintWriter(new FileWriter(indexFile));
+            System.out.println(completeFile + " - " + library.getTracks().size());
             PrintWriter out = new PrintWriter(new FileWriter(completeFile));
-            itReader.printTo(library,out);
+            Config conf = new Config();
+            conf.display.add("grouping");
+            conf.display.add("genre");
+            itReader.printTo(library,out, conf);
             out.close();
 
-            for(String genre : genres.keySet()) {
-                String grouping = genres.get(genre).get(0).getGrouping();
-                out = new PrintWriter(new FileWriter(getChildFile(completeFile, grouping, "/", genre, "-", DATE.format(new Date()), ".csv")));
-                itReader.printHeader(out);
-                itReader.printTo(genres.get(genre),library,out);
+            for(Genre genre : genres.keySet()) {
+                conf = configuration.get(genre);
+                if(conf == null) {
+                    conf = configuration.get(Genre.NIL);
+                }
+
+                File childFile = getChildFile(completeFile, genre.getGrouping(), "/", genre.getName(), "-", DATE.format(new Date()), ".csv");
+                System.out.println(childFile + " - " + genres.get(genre).size());
+                out = new PrintWriter(new FileWriter(childFile));
+                itReader.printHeader(out, conf.display);
+                itReader.printTo(genres.get(genre), library, out, conf);
                 out.close();
             }
+
+            for(Genre grouping : groups.keySet()) {
+                conf = configuration.get(grouping);
+                if(conf == null) {
+                    conf = configuration.get(Genre.NIL);
+                }
+
+                File childFile = getChildFile(completeFile, grouping.getGrouping(), "-", DATE.format(new Date()), ".csv");
+                out = new PrintWriter(new FileWriter(childFile));
+                itReader.printHeader(out, conf.display);
+                itReader.printTo(groups.get(grouping), library, out, conf);
+                out.close();
+
+                index.append(format(
+                        "<div class=%grouping% id=%group-{0}%><a href=%{2}% class=%title%>{0}</a>",
+                        grouping.getGrouping(), "", "LINK"));
+                for(Genre genre : groupsToGenres.get(grouping)) {
+                    index.append(format(
+                            "<div class=%genre% id=%genre-{0}%><a href=%{2}% class=%title%>{0}</a><span class=%count%>{1}</span>",
+                            genre.getName(), genres.get(genre).size(), "LINK"));
+                    index.append("</div>");
+                }
+                index.append("</div>");
+            }
+            index.close();
         } catch(Exception e) {
+            e.printStackTrace();
             e.printStackTrace(error);
         } catch(Throwable t) {
             t.printStackTrace();
             t.printStackTrace(error);
+            throw t;
         }
         error.close();
         errorFile.close();
@@ -67,6 +157,10 @@ public final class ITunes {
         if(empty) {
             errorF.delete();
         }
+    }
+
+    private static String format(String format, Object... args) {
+        return MessageFormat.format(format,args).replaceAll("%", "\"");
     }
 
     private static File getChildFile(File parent, String ... parts) {
@@ -98,25 +192,34 @@ public final class ITunes {
         return file;
     }
 
-
-    public ITunes(String ... displayKeys) {
-        List<String> columns = new ArrayList<String>();
-        List<String> order = new ArrayList<String>();
-
-        for(String s : displayKeys) {
-            if(s.startsWith("*")) {
-                order.add(s.substring(1));
-            } else if(s.startsWith("#")) {
-                columns.add(s.substring(1));
-            }else {
-                order.add(s);
-                columns.add(s);
-            }
-        }
-
-        display = ImmutableList.copyOf(columns);
-        sort = new BeanPropertyComparator(ImmutableList.copyOf(order));
+    public ITunes() {
+//        display = null;
+//        sort = null;
     }
+
+//    public ITunes(List<String> displayKeys, List<String> sortKeys) {
+//        display = ImmutableList.copyOf(displayKeys);
+//        sort = new BeanPropertyComparator(ImmutableList.copyOf(sortKeys));
+//    }
+//
+//    public ITunes(String ... displayKeys) {
+//        List<String> columns = new ArrayList<String>();
+//        List<String> order = new ArrayList<String>();
+//
+//        for(String s : displayKeys) {
+//            if(s.startsWith("*")) {
+//                order.add(s.substring(1));
+//            } else if(s.startsWith("#")) {
+//                columns.add(s.substring(1));
+//            }else {
+//                order.add(s);
+//                columns.add(s);
+//            }
+//        }
+//
+//        display = ImmutableList.copyOf(columns);
+//        sort = new BeanPropertyComparator(ImmutableList.copyOf(order));
+//    }
 
     public MusicLibrary read(File file) throws FileNotFoundException, XMLStreamException {
         PLEventReader itr = new PLEventReader(new FileReader(file));
@@ -138,31 +241,40 @@ public final class ITunes {
         return library;
     }
 
-    public void printTo(MusicLibrary library, PrintWriter out) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
-        printHeader(out);
-        printTo(library.getTracks().values(), library, out);
+    public void printTo(MusicLibrary library, PrintWriter out, Config conf) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        printHeader(out, conf.display);
+        printTo(library.getTracks().values(), library, out, conf);
     }
 
-    public void printHeader(PrintWriter out) {
-        for(String s : display) {
+//    public void printHeader(PrintWriter out) {
+//        printHeader(out,display);
+//    }
+
+    public void printHeader(PrintWriter out, List<String> displayKeys) {
+        for(String s : displayKeys) {
             out.print(s);
             out.print(",");
         }
         out.println();
     }
 
-    public void printTo(Collection<? extends MusicTrackReference> tracks, MusicLibrary library, PrintWriter out) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+//    public void printTo(Collection<? extends MusicTrackReference> tracks, MusicLibrary library, PrintWriter out) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+//        printTo(tracks, library, out, display, sort, (m,k) -> m.get(k) );
+//    }
 
-        TreeSet<MusicTrackReference> sortedTracks = new TreeSet<MusicTrackReference>(sort);
+    public void printTo(Collection<? extends MusicTrackReference> tracks, MusicLibrary library, PrintWriter out, Config conf) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+
+        TreeSet<MusicTrackReference> sortedTracks = new TreeSet<MusicTrackReference>(conf.sort);
         sortedTracks.addAll(tracks);
 
         for(MusicTrackReference trackReference : sortedTracks) {
             MusicTrack to = trackReference.getTrack(library);
             Map track = BeanUtils.describe(to);
-            for(String s : display) {
-                Object o = track.get(s);
+            for(String s : conf.display) {
+                Object o = conf.fallbackFunc.apply(track, s);
                 if(o != null) {
                     String ov = o.toString();
+                    ov = ov.replaceAll("\"","''");
                     if(ov.indexOf(',') > -1) {
                         out.print('"');
                         out.print(ov);
@@ -175,5 +287,74 @@ public final class ITunes {
             }
             out.println();
         }
+    }
+
+    static class Config {
+
+        public Map<String,String> fallback;
+        public BiFunction<Map<String, String>, String, String> fallbackFunc = (m,k1) -> {
+            String k = k1;
+            String v = null;
+            while(k != null) {
+                v = m.get(k);
+                if(v != null && v.length() > 0) break;
+                k = fallback.get(k);
+            }
+            if(DEBUG && k != k1) v = v + "(" + k + ")";
+            return v;
+        };
+        public List<String> display;
+        public BeanPropertyComparator sort;
+        public Map<String,Function<String,String>> format;
+
+
+
+        public Config() {
+            fallback = new HashMap<>(ImmutableMap.of(
+//                    "albumArtist", "composer",
+//                    "composer", "artist",
+                    "", ""
+            ));
+            display = new ArrayList<>(Arrays.asList(
+                    "artist",
+                    "name"
+//                    "composer",
+//                    "album",
+//                    "year"
+            ));
+            /*display = ImmutableList.of(
+                    "artist",
+                    "album",
+                    "name"
+            );/**/
+            sort = new BeanPropertyComparator(ImmutableList.of(
+//                    "genre",
+//                    "grouping",
+//                    "composer",
+                    "artist",
+                    "name"
+            ));
+            /*sort = new BeanPropertyComparator(ImmutableList.of(
+                    "genre",
+//                    "year",
+                    "grouping",
+//                    "albumArtist",
+                    "album",
+                    "trackNumber"
+            ));/**/
+            final double minutesPerMilli = 1.0 / 60 / 1000;
+            format = ImmutableMap.of(
+                    "totalTime", it->String.valueOf(Integer.parseInt(it) * minutesPerMilli),
+                    "", Function.identity()
+            );
+        }
+    }
+
+    @Data
+    static class Genre {
+        public static final Genre NIL = new Genre("","");
+
+        private final String name;
+        private final String grouping;
     }
 }
